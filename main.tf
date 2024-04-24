57e07a32b8b77f3556c1cefb78372ddb00f9ad21
Kamil Miszel <kamil.miszel@nordcloud.com>
2024-04-24 13:45:38 +0200

    addrunners

--- main.tf
+++ main.tf
@@ -1,28 +1,3 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = ">= 3.8.0"
-    }
-  }
-
-  backend "azurerm" {
-    resource_group_name  = "rg-githubtfstates"
-    storage_account_name = "miszelsharedservices"
-    container_name       = "tfstate"
-    key                  = "terraform.tfstate"
-    use_oidc             = true
-    subscription_id      = "f80611eb-0851-4373-b7a3-f272906843c4"
-    tenant_id            = "48c383d8-47c5-48f9-9e8b-afe4f2519054"
-  }
+module "rg-runners" {
+  source = ".\rg"
 }
-
-provider "azurerm" {
-  features {}
-  use_oidc = true
-}
-
-# resource "azurerm_resource_group" "rg-aks" {
-#   name     = var.resource_group_name
-#   location = var.location
-# }
