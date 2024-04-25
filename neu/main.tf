ab311198875c9247d2b2fc149f53911b48289cce
Kamil Miszel <kamil.miszel@nordcloud.com>
2024-04-25 16:53:41 +0200

    addvnet

--- neu/main.tf
+++ neu/main.tf
@@ -8,9 +8,9 @@ module "vnet" {
   source              = "./vnet"
   resource_group_name = module.rg.resource_group_location
   vnet_name           = "vnet-neu"
-  address_space       = "10.0.0.0/16"
+  address_space       = ["10.0.0.0/16"]
   subnet_name         = "default"
-  subnet_prefixes     = "10.0.0.0/24"
+  subnet_prefixes     = ["10.0.0.0/24"]
   nsg_name            = "nsg-weu"
   location            = module.rg.resource_group_location
 }
