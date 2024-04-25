92032d8370d3ea9f89ddba6db18b8e661afaa1c7
Kamil Miszel <kamil.miszel@nordcloud.com>
2024-04-25 14:36:46 +0200

    addrgs

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
 module "resource_group_rg-runner-weu" {
-  source              = "./resource_group"
+  source              = "./rg"
   resource_group_name = var.resource_group_names["rg1"]
   location            = "westeurope"
 }
 
 module "resource_group_rg-runner-neu" {
-  source              = "./resource_group"
+  source              = "./rg"
   resource_group_name = var.resource_group_names["rg2"]
   location            = "northeurope"
 }
