fd305ede12a5fcd2b9a2d5865b9a478361b2fe3c
Kamil Miszel <kamil.miszel@nordcloud.com>
2024-04-25 14:16:26 +0200

    addrgs

--- main.tf
+++ main.tf
@@ -1,3 +1,10 @@
 module "resgroups" {
   source = "./rg"
 }
+
+module "laws" {
+  source    = "./laws"
+  location  = module.rg.location
+  rg_name   = module.rg.name
+  logs_name = "logs"
+}
---
+++ rg/outputs.tf
@@ -0,0 +1,7 @@
+output "rg-runner-weu_name" {
+  value = azurerm_resource_group.rg.name
+}
+
+output "rg-runner-neu_name" {
+  value = azurerm_resource_group.rg.name
+}
